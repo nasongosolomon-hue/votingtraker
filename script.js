895be104258fcd5f2c27e9ba62a6888e1a8f8781
Solomon Nasongo <nasongosolomon@gmail.com>
2026-05-14 15:45:16 +0300

    feat(script.js)work on data initialization

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ *  Political Voting System
+ 
+ */
+
+// 1. DATA INITIALIZATION
+// Retrieve votes from localStorage or start with an empty array
+let electionData = JSON.parse(localStorage.getItem('electionData')) || [];
+
