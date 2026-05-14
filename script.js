85a34b9b85cfe4de47906a6a53306c3a27055486
Solomon Nasongo <nasongosolomon@gmail.com>
2026-05-14 15:47:37 +0300

    feat(script.js) do page specific logic

--- script.js
+++ script.js
@@ -7,3 +7,16 @@
 // Retrieve votes from localStorage or start with an empty array
 let electionData = JSON.parse(localStorage.getItem('electionData')) || [];
 
+// 2. PAGE-SPECIFIC LOGIC
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Check which page we are currently on
+    if (document.getElementById('votingForm')) {
+        initVotingPage();
+    } 
+    
+    if (document.getElementById('results-list')) {
+        initResultsPage();
+    }
+});
+
